f55cf7f2216dde9a50a8ded93a6a8816f586e6bb
Fabian Szabo <mail@fabianszabo.com>
2026-03-02 17:17:18 +0100

    test(auth): add canActivate unit tests

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,7 +1,42 @@
+import { ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
+  const authGuard: AuthGuard = new AuthGuard();
+
   it('should be defined', () => {
     expect(new AuthGuard()).toBeDefined();
+  });
+
+  describe('canActivate', () => {
+    it('should return true for valid API key', () => {
+      const mockRequest = {
+        headers: {
+          'x-api-key': 'nobody should ever commit a secret key like this',
+        },
+      };
+      const mockContext = {
+        switchToHttp: () => ({
+          getRequest: () => mockRequest,
+        }),
+      } as ExecutionContext;
+
+      expect(authGuard.canActivate(mockContext)).toBe(true);
+    });
+
+    it('should return false for invalid API key', () => {
+      const mockRequest = {
+        headers: {
+          'x-api-key': 'invalid-api-key',
+        },
+      };
+      const mockContext = {
+        switchToHttp: () => ({
+          getRequest: () => mockRequest,
+        }),
+      } as ExecutionContext;
+
+      expect(authGuard.canActivate(mockContext)).toBe(false);
+    });
   });
 });
